ed23718da379999733e38156b20958d8e9eda230
Dmitri K <69704069+DtPeach3707@users.noreply.github.com>
2022-04-15 17:36:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/quote', (req, res, next) => {
     quote(req, res, next);
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'))
  });
 
